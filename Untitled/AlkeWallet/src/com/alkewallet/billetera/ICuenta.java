3feef72672b0636d40cb2d2a7e09dd981a91013f
Gilbert Lagos <gilbert.ata@gmail.com>
2024-05-12 17:37:45 -0400

    Delete Untitled/AlkeWallet directory

--- Untitled/AlkeWallet/src/com/alkewallet/billetera/ICuenta.java
+++
@@ -1,7 +0,0 @@
-package com.alkewallet.billetera;
-
-public interface ICuenta {
-		public void retirar(Double cantidad);
-		public double consultaSaldo();
-		public void depositar(Double cantidad);
-}
--- Untitled/AlkeWallet/src/com/alkewallet/billetera/IMoneda.java
+++
@@ -1,9 +0,0 @@
-package com.alkewallet.billetera;
-public interface IMoneda {
-
-public String getNombre();
-public Double getValor();
-public String getSimbol();
-public String getPluralName();
-
-}
